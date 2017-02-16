ce875431de4c238dae2335417bdff99cd5263651
Jaden42 <jadenhill100@yahoo.com>
2017-02-16 16:32:24 -0700

    greeting() function completed and functional.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import math
 import random
+import thesaurus
+
+
+# This will create a random greeting and bias that greeting so it is more likely to be used in the future.
+def greeting():
+    result = ""
+    for phrase in thesaurus.greetings.keys():
+        prob = random.randint(thesaurus.greetings[phrase], 10)
+        if prob == 10:
+            result = phrase
+    if result == "":
+        greeting()
+    elif result != "":
+        if thesaurus.greetings[phrase] < 8:
+            thesaurus.greetings[phrase] += 1
+        return result
+
+
+print(greeting())
+print(thesaurus.greetings)
--- thesaurus.py
+++ thesaurus.py
@@ -1,2 +1,2 @@
-greetings = ["hello", 'hi', 'yo', 'ayo', 'sup', 'wassup', "what's up", 'greetings', 'howdy', 'good day', 'good morning',
-             'hi ya']
+greetings = {"hello": 1, 'hi': 1, 'yo': 1, 'ayo': 1, 'sup': 1, 'wassup': 1, "what's up": 1, 'greetings': 1,
+             'howdy': 1, 'good day': 1, 'good morning': 1, 'hi ya': 1}
