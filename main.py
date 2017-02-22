f53c90b91fff3a088d6606eeabad98b67fc300ab
Jaden42 <jadenhill100@yahoo.com>
2017-02-21 20:28:18 -0700

    Fixed it....again.

--- main.py
+++ main.py
@@ -5,24 +5,20 @@ import sentence_analysis
 
 
 # This will choose a random word or phrase and bias that phrase so it is more likely to be used in the future.
-def baisedspeech(dict):
-    result = None
-    for phrase in dict.keys():
-        prob = random.randint(dict[phrase], 10)
-        if prob == 10:
-            result = phrase
-            break
-    if result is None:
-        print('try again')
-        baisedspeech(dict)
-    else:
-        if dict[result] < 8:
-            dict[result] += 1
-        print('got result')
-        return result
+def biasedspeech(dictionary):
+    weightIndex = 0
+    totalWeight = 0
+    for n in dictionary.values():
+        totalWeight += n
+    choice = random.randint(1, totalWeight)
+    for phrase in dictionary:
+        weightIndex += dictionary[phrase]
+        if weightIndex >= choice:
+            dictionary[phrase] += 1
+            return phrase
 
 
-print(baisedspeech(thesaurus.greetings))
+print(biasedspeech(thesaurus.greetings))
 
 while True:
     userInput = input().lower()
